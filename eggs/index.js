34a2870987d5bc8d3b16545271bdfbb3cdcb73b3
Carloz2er <73202676+Carloz2er@users.noreply.github.com>
2025-10-02 10:43:12 -0400

    Update index.js

--- eggs/index.js
+++ eggs/index.js
@@ -1,9 +1,17 @@
 const puppeteer = require("puppeteer");
 
+const puppeteerArgs = process.env.PUPPETEER_ARGS ? 
+                      process.env.PUPPETEER_ARGS.split(' ') : 
+                      ['--no-sandbox', '--disable-setuid-sandbox'];
+
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://google.com");
-  console.log("Título da página:", await page.title());
-  await browser.close();
+    const browser = await puppeteer.launch({
+        headless: "new", 
+        args: puppeteerArgs, 
+    });
+    
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+    console.log("Título da página:", await page.title());
+    await browser.close();
 })();
