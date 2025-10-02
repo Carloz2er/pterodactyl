0aa52ab541329e32c83ee3d8d7653cf3d6d0f5f4
Carloz2er <73202676+Carloz2er@users.noreply.github.com>
2025-10-02 10:46:29 -0400

    Update index.js

--- eggs/index.js
+++ eggs/index.js
@@ -1,13 +1,17 @@
 const puppeteer = require("puppeteer");
 
-const puppeteerArgs = process.env.PUPPETEER_ARGS ? 
-                      process.env.PUPPETEER_ARGS.split(' ') : 
-                      ['--no-sandbox', '--disable-setuid-sandbox'];
+const puppeteerArgs = process.env.PUPPETEER_ARGS 
+                      ? process.env.PUPPETEER_ARGS.split(' ') 
+                      : ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'];
+
+const executablePath = process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser';
+
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: "new", 
-        args: puppeteerArgs, 
+        headless: "new",
+        executablePath: executablePath,
+        args: puppeteerArgs,
     });
     
     const page = await browser.newPage();
